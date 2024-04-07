6e2433337e0c7bb59997c61eef26b44fb5353a87
Bahtiar-Za-russia <162881804+Bahtiar-Za-russia@users.noreply.github.com>
2024-04-07 15:56:34 +0500

    yhdhav
    jytdj

--- cons.js
+++ cons.js
@@ -1,41 +1,35 @@
-class Bird {
-    constructor(name, voice, canFly){
-        this.name = name;
-        this.voice = voice;
-        this.canFly = canFly;
-    }
-    say(){
-        console.log(this.name, 'goes', this.voice);
-    }
-    fly(){
-        console.log(this.name, 'flying hight');
-    }
-}
+// class Bird {
+//     constructor(name, voice, canFly){
+//         this.name = name;
+//         this.voice = voice;
+//         this.canFly = canFly;
+//     }
+//     say(){
+//         console.log(this.name, 'goes', this.voice);
+//     }
+//     fly(){
+//         console.log(this.name, 'flying hight');
+//     }
+// }
 
-class Duck extends Bird{
-    constructor(name, voice, canFly){
-        super(name, voice, canFly);
-    }
-}
+// class Duck extends Bird{
+//     constructor(name, voice, canFly){
+//         super(name, voice, canFly);
+//     }
+// }
 
-const myDuck = new Duck('Utka Finlandiavna', 'quack', true);
-myDuck.say();
-myDuck.fly();
+// const myDuck = new Duck('Utka Finlandiavna', 'quack', true);
+// myDuck.say();
+// myDuck.fly();
 
-class Duck extends Bird{
-    constructor(name, voice, canFly){
-        super(name, voice, canFly)
-    }
-    say(){
-        console.log(this.name, 'stays silent.');
-    }
-}
-
-
-
-
-
-
+// class Duck extends Bird{
+//     constructor(name, voice, canFly){
+//         super(name, voice, canFly)
+//     }
+//     say(){
+//         console.log(this.name, 'stays silent.');
+//     }
+// }
 
 
 
@@ -54,22 +48,22 @@ class Duck extends Bird{
 // dog.say();
 // cat.say();
 
-// const animal = {
-//     say(){
-//         console.log(this.name, 'goes', this.voice);
-//     }
-// }
-// function createAnimal(name, voice){
-//     const result = Object.create(animal);
-//     result.name = name;
-//     result.voice = voice;
-//     return result;
-// }
-// const dog = createAnimal('Dog', 'woof');
-// const cat = createAnimal('Cat', 'meow');
+const animal = {
+    say(){
+        console.log(this.name, 'goes', this.voice);
+    }
+}
+function createAnimal(name, voice){
+    const result = Object.create(animal);
+    result.name = name;
+    result.voice = voice;
+    return result;
+}
+const dog = createAnimal('churchil', 'sfegrewsfre');
+const cat = createAnimal('Cat', 'meow');
 
-// dog.say();
-// cat.say();
+dog.say();
+cat.say();
 
 
 // const animal = {
